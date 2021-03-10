ba0fe8b3e434c0d24c2f37eaf688fd4bb91ac6b3
Douglas Abrams <abramsd@juno.cbio.private>
2021-03-10 15:20:46 -0500

    fix:roh reader

--- wgs/workflows/sample_qc/tasks.py
+++ wgs/workflows/sample_qc/tasks.py
@@ -97,9 +97,7 @@ def prep_sv_for_circos(sv_calls, outfile):
     svs["color"] = svs.rearrangement_type.replace(types, colors)
 
     svs.to_csv(outfile, index=False, header=True, sep="\t")
-
-
- def parse_roh(roh_calls, parsed): 
+def parse_roh(roh_calls, parsed):
     if roh_calls.endswith(".gz"):
         lines = [l for l in gzip.open(roh_calls, "rt") if "ST" in l]
     else:
